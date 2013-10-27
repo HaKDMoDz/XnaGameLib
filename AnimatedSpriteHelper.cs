0d72916867a1a07df322ad618b591a08ea72db2c
Tyson Grant Nottingham <tgnottingham@gmail.com>
2013-10-26 18:44:29 -0700

    ** Added AnimatedSpriteHelper class.

---
+++ AnimatedSpriteHelper.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace XnaGameLib
+{
+	public static class AnimatedSpriteHelper
+	{
+		public static AnimatedSprite<T> CreateAnimatedSprite<T>(Texture2D texture, int rows, int columns, int frames,
+		                                                        TimeSpan frameDuration, AnimationHelper.Order order,
+		                                                        Vector2 scale, Vector2 frameOrigin, T key)
+		{
+			Dictionary<T, Animation> animations = new Dictionary<T, Animation>();
+			Animation animation = AnimationHelper.CreateAnimation(texture, rows, columns, frames, frameDuration, order);
+			animations.Add(key, animation);
+			AnimatedSprite<T> sprite = new AnimatedSprite<T>(animations, key);
+			sprite.TextureOrigin = frameOrigin;
+			sprite.Scale = scale;
+			return sprite;
+		}
+	}
+}
+
